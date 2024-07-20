2087bba7e28a632a8d7d5a2dae46289bb3c3a146
Brody Brand <80013712+brodybrand@users.noreply.github.com>
2024-07-20 13:52:16 -0700

    feat: added images and basic ds for cards

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 A simple memory game using the React library.
+
+Credits:
+-Card Images https://github.com/jhcheung/slay-the-spire-api
---
+++ src/data.js
@@ -0,0 +1,104 @@
+const cardData = {
+  card_1: {
+    id: 1,
+    name: "Accuracy",
+    image: "src/assets/card-images/the-silent-deck/Accuracy.png",
+  },
+  card_2: {
+    id: 2,
+    name: "Acrobatics",
+    image: "src/assets/card-images/the-silent-deck/Acrobatics.png",
+  },
+  card_3: {
+    id: 3,
+    name: "Adrenaline",
+    image: "src/assets/card-images/the-silent-deck/Adrenaline.png",
+  },
+  card_4: {
+    id: 4,
+    name: "After Image",
+    image: "src/assets/card-images/the-silent-deck/AfterImage.png",
+  },
+  card_5: {
+    id: 5,
+    name: "Alchemize",
+    image: "src/assets/card-images/the-silent-deck/Alchemize.png",
+  },
+  card_6: {
+    id: 6,
+    name: "All-Out Attack",
+    image: "src/assets/card-images/the-silent-deck/All-OutAttack.png",
+  },
+  card_7: {
+    id: 7,
+    name: "A Thousand Cuts",
+    image: "src/assets/card-images/the-silent-deck/AThousandCuts.png",
+  },
+  card_8: {
+    id: 8,
+    name: "Backflip",
+    image: "src/assets/card-images/the-silent-deck/Backflip.png",
+  },
+  card_9: {
+    id: 9,
+    name: "Backstab",
+    image: "src/assets/card-images/the-silent-deck/Backstab.png",
+  },
+  card_10: {
+    id: 10,
+    name: "Bane",
+    image: "src/assets/card-images/the-silent-deck/Bane.png",
+  },
+  card_11: {
+    id: 11,
+    name: "Blade Dance",
+    image: "src/assets/card-images/the-silent-deck/BladeDance.png",
+  },
+  card_12: {
+    id: 12,
+    name: "Blur",
+    image: "src/assets/card-images/the-silent-deck/Blur.png",
+  },
+  card_13: {
+    id: 13,
+    name: "Bouncing Flask",
+    image: "src/assets/card-images/the-silent-deck/BouncingFlask.png",
+  },
+  card_14: {
+    id: 14,
+    name: "Bullet Time",
+    image: "src/assets/card-images/the-silent-deck/BulletTime.png",
+  },
+  card_15: {
+    id: 15,
+    name: "Burst",
+    image: "src/assets/card-images/the-silent-deck/Burst.png",
+  },
+  card_16: {
+    id: 16,
+    name: "Calculated Gamble",
+    image: "src/assets/card-images/the-silent-deck/CalculatedGamble.png",
+  },
+  card_17: {
+    id: 17,
+    name: "Caltrops",
+    image: "src/assets/card-images/the-silent-deck/Caltrops.png",
+  },
+  card_18: {
+    id: 18,
+    name: "Catalyst",
+    image: "src/assets/card-images/the-silent-deck/Catalyst.png",
+  },
+  card_19: {
+    id: 19,
+    name: "Choke",
+    image: "src/assets/card-images/the-silent-deck/Choke.png",
+  },
+  card_20: {
+    id: 20,
+    name: "Cloak and Dagger",
+    image: "src/assets/card-images/the-silent-deck/CloakandDagger.png",
+  },
+};
+
+export default cardData;
